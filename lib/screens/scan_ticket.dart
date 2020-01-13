8c9ee6032a9b24e6c8d1d67f2689752ec40b440e
Butzlabben <darkstyler@web.de>
2020-01-13 15:48:16 +0100

    Adjusted some iOS stuff

--- lib/screens/scan_ticket.dart
+++ lib/screens/scan_ticket.dart
@@ -105,6 +105,7 @@ class _InputWidgetState extends State<InputWidget> {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) {
       widget.scanCallback(scanData);
+      controller.dispose();
     });
   }
 
